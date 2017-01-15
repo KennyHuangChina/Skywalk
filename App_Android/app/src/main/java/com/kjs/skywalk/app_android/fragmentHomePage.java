69cabed55b30ee9bd0139d24bd4a96a9751767c2
sailorzhou <shuguang168@sina.com>
2017-01-15 17:16:35 +0800

    [Android App] 添加房屋信息列表

--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/fragmentHomePage.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/fragmentHomePage.java
@@ -1,17 +1,20 @@
 package com.kjs.skywalk.app_android;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * Created by sailor.zhou on 2017/1/11.
  */
 
 public class fragmentHomePage extends Fragment {
+    private Context mContext;
     public fragmentHomePage() {
         super();
     }
@@ -19,6 +22,17 @@ public class fragmentHomePage extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_homepage, container, false);
+        View view = inflater.inflate(R.layout.fragment_homepage, container, false);
+        ListView lvContent = (ListView) view.findViewById(R.id.lv_content);
+
+        homepage_apartment_listitem_adapter adapter = new homepage_apartment_listitem_adapter(mContext);
+        lvContent.setAdapter(adapter);
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
     }
 }
---
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/homepage_apartment_listitem_adapter.java
@@ -0,0 +1,57 @@
+package com.kjs.skywalk.app_android;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by sailor.zhou on 2017/1/15.
+ */
+
+public class homepage_apartment_listitem_adapter extends BaseAdapter {
+    private Context mContext;
+
+    public homepage_apartment_listitem_adapter(Context context) {
+        super();
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    class ViewHolder {
+        TextView tvContentName;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder holder;
+
+        if (view == null) {
+            view = LayoutInflater.from(mContext).inflate(R.layout.homepage_listitem, null);
+
+            holder = new ViewHolder();
+            holder.tvContentName = (TextView) view.findViewById(R.id.tv_info_title);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+
+        return view;
+    }
+}
