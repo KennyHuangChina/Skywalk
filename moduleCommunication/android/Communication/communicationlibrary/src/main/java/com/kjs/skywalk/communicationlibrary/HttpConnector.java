791359fef8d104d3ddafde63f78a12f58beeaecc
Jackie.Guo <bigbigpigpig@163.com>
2017-01-17 15:46:17 +0800

    add new empty communication mudule for android

---
+++ moduleCommunication/android/Communication/communicationlibrary/src/main/java/com/kjs/skywalk/communicationlibrary/HttpConnector.java
@@ -0,0 +1,14 @@
+package com.kjs.skywalk.communicationlibrary;
+
+import android.content.Context;
+
+/**
+ * Created by Jackie on 2017/1/17.
+ */
+
+public class HttpConnector {
+    private Context mContext = null;
+    public HttpConnector(Context context) {
+        mContext = context;
+    }
+}
---
+++ moduleCommunication/android/Communication/communicationlibrary/src/main/java/com/kjs/skywalk/communicationlibrary/InternalDefines.java
@@ -0,0 +1,17 @@
+package com.kjs.skywalk.communicationlibrary;
+
+/**
+ * Created by Jackie on 2017/1/17.
+ */
+
+class InternalDefines {
+
+    //Log Tags defines
+    private final String TAG_HTTPConnector = "HTTPConnector";
+
+    //Error Code Defines
+    private final int       ERROR_CODE_OK = 0x0;
+    private final String    ERROR_DESCRIPTION_OK = "No Error";
+    //==Http Connection Error Code Defines==
+    private final int       ERROR_CODE_HTTP_CONNECTION_INVALID_URL = 0x80001000;
+}
