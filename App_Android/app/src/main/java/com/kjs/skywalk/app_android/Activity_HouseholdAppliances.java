6db20369a41d3f802cb8559deb296839e97eb1f0
Sailor.Zhou <shuguang168@sina.com>
2017-02-03 11:20:06 +0800

    [Android App] 调整房屋设施页面的显示大小

--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_HouseholdAppliances.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_HouseholdAppliances.java
@@ -15,12 +15,12 @@ public class Activity_HouseholdAppliances extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__household_appliances);
 
-        SpannableString styleText = new SpannableString("世贸蝶湖湾175栋2202室");
+        SpannableString styleText = new SpannableString("世贸蝶湖湾 175栋2202室");
         int i = styleText.length();
-        String s = "世贸蝶湖湾175栋2202室";
+        String s = "世贸蝶湖湾 175栋2202室";
         int l = s.length();
-        styleText.setSpan(new TextAppearanceSpan(this, R.style.textstyle_large), 0, 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        styleText.setSpan(new TextAppearanceSpan(this, R.style.textstyle_small), 5, 14, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        styleText.setSpan(new TextAppearanceSpan(this, R.style.textstyle_large), 0, 5, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        styleText.setSpan(new TextAppearanceSpan(this, R.style.textstyle_small), 5, 15, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         ((TextView)findViewById(R.id.tv_apartment_name)).setText(styleText, TextView.BufferType.SPANNABLE);
 
         ListView lvHouseApp = (ListView)findViewById(R.id.lvHouseholdAppliance);
