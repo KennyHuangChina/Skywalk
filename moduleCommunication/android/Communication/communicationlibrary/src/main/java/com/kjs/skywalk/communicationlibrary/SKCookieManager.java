d5e88db00c8ee375c0b5693d0ceaef2850b6d7df
Jackie.Guo <bigbigpigpig@163.com>
2017-02-13 23:20:58 +0800

    use sharedprefference to save cookie from server

--- moduleCommunication/android/Communication/communicationlibrary/src/main/java/com/kjs/skywalk/communicationlibrary/SKCookieManager.java
+++ moduleCommunication/android/Communication/communicationlibrary/src/main/java/com/kjs/skywalk/communicationlibrary/SKCookieManager.java
@@ -2,19 +2,53 @@ package com.kjs.skywalk.communicationlibrary;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.hardware.camera2.params.StreamConfigurationMap;
-import android.os.Build;
 import android.util.Log;
-import android.webkit.CookieManager;
-import android.webkit.CookieSyncManager;
-import android.webkit.ValueCallback;
-import android.webkit.WebView;
 
 /**
  * Created by Jackie on 2017/1/25.
  */
 
 class SKCookieManager {
+
+    private static SKCookieManager mSKManager = null;
+    private Context mContext = null;
+
+    private SKCookieManager(Context context) {
+        mContext = context;
+    }
+
+    public static synchronized SKCookieManager getManager(Context context) {
+        if(mSKManager == null) {
+            mSKManager = new SKCookieManager(context);
+        }
+
+        return mSKManager;
+    }
+
+    public void setCookie(String url, String value) {
+        Log.i(InternalDefines.TAG_COMMUNICATION_COOKIE_MANAGER, "setCookie: ");
+        Log.i(InternalDefines.TAG_COMMUNICATION_COOKIE_MANAGER, "\tURL->" + url);
+        Log.i(InternalDefines.TAG_COMMUNICATION_COOKIE_MANAGER, "\tCookie->" + value);
+        if(url == null || value == null) {
+            return;
+        }
+
+        SharedPreferences sp= mContext.getSharedPreferences("Cookies", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("Session-Cookie", value);
+        editor.commit();
+    }
+
+    public String getCookie(String url) {
+        Log.i(InternalDefines.TAG_COMMUNICATION_COOKIE_MANAGER, "getCookie: ");
+        Log.i(InternalDefines.TAG_COMMUNICATION_COOKIE_MANAGER, "\tURL->" + url);
+        SharedPreferences sp= mContext.getSharedPreferences("Cookies", Context.MODE_PRIVATE);
+        String cookie = sp.getString("Session-Cookie", "");
+        Log.i(InternalDefines.TAG_COMMUNICATION_COOKIE_MANAGER, "\tCookie->" + cookie);
+        return cookie;
+    }
+
+    /*
     private static SKCookieManager mSKManager = null;
     private Context mContext = null;
     private CookieManager mManager = null;
@@ -50,4 +84,5 @@ class SKCookieManager {
         Log.i(InternalDefines.TAG_COMMUNICATION_COOKIE_MANAGER, "\tCookie->" + cookie);
         return cookie;
     }
+    */
 }
