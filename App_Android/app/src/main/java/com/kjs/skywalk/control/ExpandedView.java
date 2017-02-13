6703651c46ef2f02135a043a66d280084faa154d
sailorzhou <shuguang168@sina.com>
2017-02-13 23:00:21 +0800

    [Android App] 增加几种分类的style

---
+++ App_Android/app/src/main/java/com/kjs/skywalk/control/ExpandedView.java
@@ -0,0 +1,24 @@
+package com.kjs.skywalk.control;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by sailor.zhou on 2017/2/13.
+ */
+
+// http://www.cnblogs.com/JczmDeveloper/p/3782586.html
+public class ExpandedView extends FrameLayout {
+    public ExpandedView(Context context) {
+        super(context);
+    }
+
+    public ExpandedView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ExpandedView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
