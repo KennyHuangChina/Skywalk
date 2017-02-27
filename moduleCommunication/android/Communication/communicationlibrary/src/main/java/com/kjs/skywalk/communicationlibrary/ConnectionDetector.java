306e8e76bf8502dbdcb6860a06054e388b0a675d
Kenny.Huang <2465691298@qq.com>
2017-02-27 22:49:28 +0800

    <communication lib> 增加网络状态检查 1. 提交 new class，ConnectionDetector.java，用以检查网络状态 2. 上次提交，还少了记录：解决一个在 GetUserInfo 中，如果传入的 id 为空，会 crash 的问题，需要捕获 exception NumberFormatException

---
+++ moduleCommunication/android/Communication/communicationlibrary/src/main/java/com/kjs/skywalk/communicationlibrary/ConnectionDetector.java
@@ -0,0 +1,54 @@
+package com.kjs.skywalk.communicationlibrary;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+/**
+ * Created by kenny on 2017/2/27.
+ */
+
+class ConnectionDetector {
+    final static String TAG = "ConnectionDetector";
+    private Context mContext;
+
+    public ConnectionDetector(Context context){
+        this.mContext = context;
+    }
+
+    public boolean isConnectingToInternet(){
+        ConnectivityManager connectivity = (ConnectivityManager)mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (null == connectivity) {
+            return false;
+        }
+/*
+        NetworkInfo[] info = connectivity.getAllNetworkInfo();
+        if (null == info) {
+            return false;
+        }
+
+        for (int i = 0; i < info.length; i++) {
+            if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                return true;
+            }
+        }
+*/
+        NetworkInfo.State wifi = connectivity.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState();
+        if (wifi == NetworkInfo.State.CONNECTED) {
+            Log.i(TAG, "WiFi is connected");
+            return true;
+        }
+        Log.w(TAG, "WiFi is not connected");
+
+        NetworkInfo.State mobile = connectivity.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState();
+        if (mobile == NetworkInfo.State.CONNECTED) {
+            Log.i(TAG, "mobile is connected");
+            return true;
+        }
+        Log.w(TAG, "mobile is not connected");
+
+        return false;
+    }
+}
