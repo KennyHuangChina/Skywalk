7e2948948e208b91b37458541159703adbcc1b4d
sailorzhou <shuguang168@sina.com>
2017-03-07 22:27:54 +0800

    [Android App] 出租进度 密码输入弹出框

--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_rentals_progress_yijia.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_rentals_progress_yijia.java
@@ -1,8 +1,10 @@
 package com.kjs.skywalk.app_android;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Activity_rentals_progress_yijia extends AppCompatActivity {
 
@@ -21,6 +23,56 @@ public class Activity_rentals_progress_yijia extends AppCompatActivity {
                 finish();
             }
             break;
+
+            case R.id.tv_refuse:
+            {
+                finish();
+            }
+            break;
+
+            case R.id.tv_accept:
+            {
+                showInputPwdDlg();
+            }
+            break;
+
+            case R.id.tv_reoffer:
+            {
+                finish();
+            }
+            break;
+
+            case R.id.tv_transform:
+            {
+                finish();
+            }
+            break;
+
         }
+    }
+
+    private AlertDialog mInputPwdDlg;
+    private void showInputPwdDlg() {
+        if (mInputPwdDlg == null) {
+            mInputPwdDlg = new AlertDialog.Builder(this).create();
+        }
+        mInputPwdDlg.show();
+        mInputPwdDlg.setContentView(R.layout.dialog_rentals_progress_password);
+
+        TextView tvBack = (TextView) mInputPwdDlg.findViewById(R.id.tv_cancel);
+        tvBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mInputPwdDlg.dismiss();
+            }
+        });
+
+        TextView tvConfirm = (TextView) mInputPwdDlg.findViewById(R.id.tv_confirm);
+        tvConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mInputPwdDlg.dismiss();
+            }
+        });
     }
 }
