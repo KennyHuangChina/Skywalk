485644dbdbb9d8126f38c16d62cbff3f9935bc88
sailorzhou <shuguang168@sina.com>
2017-03-13 22:52:03 +0800

    [Android App] 签约-押金 页面

--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_rentals_progress_contractedDeposit.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_rentals_progress_contractedDeposit.java
@@ -4,12 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class Activity_rentals_progress_contractedBoth extends AppCompatActivity {
+public class Activity_rentals_progress_contractedDeposit extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rentals_progress_contracted_both);
+        setContentView(R.layout.activity_rentals_progress_contracted_deposit);
     }
 
     public void onViewClick(View v) {
