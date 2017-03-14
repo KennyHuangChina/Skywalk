f01770b26ddd7e940818e9bd5e5a13a29d70eb04
sailorzhou <shuguang168@sina.com>
2017-03-14 23:39:39 +0800

    [Android App] 签约-合同确认 页面

---
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_rentals_progress_contractedConfirm.java
@@ -0,0 +1,13 @@
+package com.kjs.skywalk.app_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_rentals_progress_contractedConfirm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rentals_progress_contracted_confirm);
+    }
+}
