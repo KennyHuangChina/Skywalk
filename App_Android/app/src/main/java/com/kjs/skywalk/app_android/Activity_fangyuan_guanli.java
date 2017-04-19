b6430e0fcf3143658f6857498a983e80189df885
sailorzhou <shuguang168@sina.com>
2017-04-20 00:29:10 +0800

    [Android App] 房源信息管理 页面

---
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_fangyuan_guanli.java
@@ -0,0 +1,13 @@
+package com.kjs.skywalk.app_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_fangyuan_guanli extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fangyuan_guanli);
+    }
+}
