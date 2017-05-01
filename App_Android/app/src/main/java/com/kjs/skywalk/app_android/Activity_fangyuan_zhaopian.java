81821f9ee286ee3e759eb8c9835a62e3815a0877
sailorzhou <shuguang168@sina.com>
2017-05-01 23:03:31 +0800

    [Android App] 房源照片页面添加内容

--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_fangyuan_zhaopian.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_fangyuan_zhaopian.java
@@ -1,9 +1,15 @@
 package com.kjs.skywalk.app_android;
 
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import com.kjs.skywalk.app_android.Homepage.fragmentHomePage;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Activity_fangyuan_zhaopian extends AppCompatActivity {
 
@@ -11,6 +17,63 @@ public class Activity_fangyuan_zhaopian extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fangyuan_zhaopian);
+
+        FragmentTransaction fragTransaction = getFragmentManager().beginTransaction();
+
+        // 户型图
+        ArrayList<fragmentFangYuanZhaoPianGroup.PicList> huXingPicLst = new ArrayList<fragmentFangYuanZhaoPianGroup.PicList> (
+                Arrays.asList(
+                        new fragmentFangYuanZhaoPianGroup.PicList("户型图一", R.drawable.huxingtu1),
+                        new fragmentFangYuanZhaoPianGroup.PicList("户型图二", R.drawable.huxingtu2)
+                )
+        );
+        fragmentFangYuanZhaoPianGroup.Group huXingGroup = new fragmentFangYuanZhaoPianGroup.Group("户型图", huXingPicLst);
+
+        fragmentFangYuanZhaoPianGroup fraHuXing = new fragmentFangYuanZhaoPianGroup();
+        fraHuXing.setGroup(huXingGroup);
+        fragTransaction.replace(R.id.fl_huxing, fraHuXing);
+
+        // 房间结构
+        ArrayList<fragmentFangYuanZhaoPianGroup.PicList> fangJianJieGouLst = new ArrayList<fragmentFangYuanZhaoPianGroup.PicList> (
+                Arrays.asList(
+                        new fragmentFangYuanZhaoPianGroup.PicList("房间结构图一", R.drawable.huxingtu1),
+                        new fragmentFangYuanZhaoPianGroup.PicList("房间结构图二", R.drawable.huxingtu2)
+                )
+        );
+        fragmentFangYuanZhaoPianGroup.Group fangJianJieGouGroup = new fragmentFangYuanZhaoPianGroup.Group("房间结构", fangJianJieGouLst);
+
+        fragmentFangYuanZhaoPianGroup fraFangJianJieGou = new fragmentFangYuanZhaoPianGroup();
+        fraFangJianJieGou.setGroup(fangJianJieGouGroup);
+        fragTransaction.replace(R.id.fl_fangjianjiegou, fraFangJianJieGou);
+
+        // 家居用品
+        ArrayList<fragmentFangYuanZhaoPianGroup.PicList> jiaJuYongPinLst = new ArrayList<fragmentFangYuanZhaoPianGroup.PicList> (
+                Arrays.asList(
+                        new fragmentFangYuanZhaoPianGroup.PicList("家居用品图一", R.drawable.huxingtu1),
+                        new fragmentFangYuanZhaoPianGroup.PicList("家居用品图二", R.drawable.huxingtu2)
+                )
+        );
+        fragmentFangYuanZhaoPianGroup.Group jiaJuYongPinGroup = new fragmentFangYuanZhaoPianGroup.Group("家居用品", jiaJuYongPinLst);
+
+        fragmentFangYuanZhaoPianGroup fraJiaJuYongPin = new fragmentFangYuanZhaoPianGroup();
+        fraJiaJuYongPin.setGroup(jiaJuYongPinGroup);
+        fragTransaction.replace(R.id.fl_jiajuyongpin, fraJiaJuYongPin);
+
+        // 电器
+        ArrayList<fragmentFangYuanZhaoPianGroup.PicList> dianQiLst = new ArrayList<fragmentFangYuanZhaoPianGroup.PicList> (
+                Arrays.asList(
+                        new fragmentFangYuanZhaoPianGroup.PicList("电器图一", R.drawable.huxingtu1),
+                        new fragmentFangYuanZhaoPianGroup.PicList("电器图二", R.drawable.huxingtu2)
+                )
+        );
+        fragmentFangYuanZhaoPianGroup.Group dianQiGroup = new fragmentFangYuanZhaoPianGroup.Group("电器", dianQiLst);
+
+        fragmentFangYuanZhaoPianGroup fraDianQi = new fragmentFangYuanZhaoPianGroup();
+        fraDianQi.setGroup(dianQiGroup);
+        fragTransaction.replace(R.id.fl_dianqi, fraDianQi);
+        //
+
+        fragTransaction.commit();
     }
 
     public void onViewClick(View v) {
--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/fragmentFangYuanZhaoPianGroup.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/fragmentFangYuanZhaoPianGroup.java
@@ -6,15 +6,60 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by sailor.zhou on 2017/1/11.
  */
 
 public class fragmentFangYuanZhaoPianGroup extends Fragment {
+    private Group mGroup;
+
+    static class PicList {
+        String mDesc;
+        int    mDrawable;
+
+        public PicList(String desc, int drawable) {
+            mDesc = desc;
+            mDrawable = drawable;
+        }
+    }
+
+    static class Group {
+        String mTitle;
+        ArrayList<PicList> mList;
+
+        public Group(String title,  ArrayList<PicList> list) {
+            mTitle = title;
+            mList = list;
+        }
+    }
+
+    public void setGroup(Group group) {
+        mGroup = group;
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fangyuanzhaopian_group, container, false);
+        View view = inflater.inflate(R.layout.fragment_fangyuanzhaopian_group, container, false);
+
+        TextView tvTitle = (TextView) view.findViewById(R.id.tv_title);
+        tvTitle.setText(mGroup.mTitle);
+
+        ImageView ivPic = (ImageView) view.findViewById(R.id.iv_pic);
+        TextView tvPicDes = (TextView) view.findViewById(R.id.tv_pic_desc);
+        ivPic.setImageResource(mGroup.mList.get(0).mDrawable);
+        tvPicDes.setText("照片说明：" + mGroup.mList.get(0).mDesc);
+
+        ImageView ivPic1 = (ImageView) view.findViewById(R.id.iv_pic1);
+        TextView tvPicDes1 = (TextView) view.findViewById(R.id.tv_pic_desc1);
+        ivPic1.setImageResource(mGroup.mList.get(1).mDrawable);
+        tvPicDes1.setText("照片说明：" + mGroup.mList.get(1).mDesc);
+
+        return view;
     }
 }
