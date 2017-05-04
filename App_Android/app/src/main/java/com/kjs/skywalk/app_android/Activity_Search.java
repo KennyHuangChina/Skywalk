3116a22a78d0f0b82ff4b83bed425e22b6b4ebe0
Jackie.Guo <bigbigpigpig@163.com>
2017-05-04 22:59:38 +0800

    <Android App> 完成页面 搜索小区1

--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_Search.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_Search.java
@@ -2,8 +2,11 @@ package com.kjs.skywalk.app_android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 /**
  * Created by admin on 2017/3/22.
@@ -15,6 +18,13 @@ public class Activity_Search extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        SearchView searchView = (SearchView)findViewById(R.id.search_view);
+        int id = searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+        TextView textView = (TextView) searchView.findViewById(id);
+        textView.setTextSize(14);
+        textView.setGravity(Gravity.BOTTOM);
+        searchView.onActionViewCollapsed();
 
         mListView = (ListView)findViewById(R.id.listViewHint);
         AdapterHouseSearchResult adapter = new AdapterHouseSearchResult(this);
