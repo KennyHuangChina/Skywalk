568372035f18c3a792c5b3f9139c510334b803e6
sailorzhou <shuguang168@sina.com>
2017-05-04 23:53:38 +0800

    [Android App] 显示照片水平滚动，点击check/uncheck.

--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_fangyuan_zhaopian.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_fangyuan_zhaopian.java
@@ -1,7 +1,5 @@
 package com.kjs.skywalk.app_android;
 
-import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -9,8 +7,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
-import com.kjs.skywalk.app_android.Homepage.fragmentHomePage;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,83 +20,50 @@ public class Activity_fangyuan_zhaopian extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fangyuan_zhaopian);
 
-        FragmentTransaction fragTransaction = getFragmentManager().beginTransaction();
-
         // 户型图
+        mVPHuXing = (ViewPager) findViewById(R.id.vp_huxing);
         ArrayList<fragmentFangYuanZhaoPianGroup.PicList> huXingPicLst = new ArrayList<fragmentFangYuanZhaoPianGroup.PicList> (
                 Arrays.asList(
-                        new fragmentFangYuanZhaoPianGroup.PicList("户型图一", R.drawable.huxingtu1),
-                        new fragmentFangYuanZhaoPianGroup.PicList("户型图二", R.drawable.huxingtu2),
-                        new fragmentFangYuanZhaoPianGroup.PicList("户型图三", R.drawable.huxingtu2),
-                        new fragmentFangYuanZhaoPianGroup.PicList("户型图四", R.drawable.huxingtu2),
-                        new fragmentFangYuanZhaoPianGroup.PicList("户型图五", R.drawable.huxingtu2),
-                        new fragmentFangYuanZhaoPianGroup.PicList("户型图六", R.drawable.huxingtu2)
+                        new fragmentFangYuanZhaoPianGroup.PicList("户型图一", R.drawable.huxingtu1, false),
+                        new fragmentFangYuanZhaoPianGroup.PicList("户型图二", R.drawable.huxingtu2, false),
+                        new fragmentFangYuanZhaoPianGroup.PicList("户型图三", R.drawable.huxingtu2, false),
+                        new fragmentFangYuanZhaoPianGroup.PicList("户型图四", R.drawable.huxingtu2, false),
+                        new fragmentFangYuanZhaoPianGroup.PicList("户型图五", R.drawable.huxingtu2, false),
+                        new fragmentFangYuanZhaoPianGroup.PicList("户型图六", R.drawable.huxingtu2, false)
              )
         );
-//        fragmentFangYuanZhaoPianGroup.Group huXingGroup = new fragmentFangYuanZhaoPianGroup.Group("户型图", huXingPicLst);
+        fillPicGroupInfo(mVPHuXing, huXingPicLst);
 
-//        fragmentFangYuanZhaoPianGroup fraHuXing = new fragmentFangYuanZhaoPianGroup();
-////        fraHuXing.setGroup(huXingGroup);
-//        fraHuXing.setPicList(huXingPicLst);
-//        fragTransaction.replace(R.id.fl_huxing, fraHuXing);
+        //        // 房间结构
+        ViewPager vpFangJianJieGou = (ViewPager) findViewById(R.id.vp_fangjianjiegou);
+        ArrayList<fragmentFangYuanZhaoPianGroup.PicList> fangJianJieGouPicLst = new ArrayList<> (
+                Arrays.asList(
+                        new fragmentFangYuanZhaoPianGroup.PicList("房间结构图一", R.drawable.huxingtu1, false),
+                        new fragmentFangYuanZhaoPianGroup.PicList("房间结构图二", R.drawable.huxingtu2, false)
+                )
+        );
+        fillPicGroupInfo(vpFangJianJieGou, fangJianJieGouPicLst);
 
-        mVPHuXing = (ViewPager) findViewById(R.id.vp_huxing);
-        List<Fragment> fragLstHuXing = new ArrayList<Fragment>();
-        int lstCnt = huXingPicLst.size() % 2 == 0 ? huXingPicLst.size() / 2 : huXingPicLst.size() / 2 + 1;
-        for (int i = 0; i <lstCnt; i++) {
-            fragmentFangYuanZhaoPianGroup fragment = new fragmentFangYuanZhaoPianGroup();
-            ArrayList<fragmentFangYuanZhaoPianGroup.PicList> list = new ArrayList();
-            list.add(huXingPicLst.get(i * 2));
-            list.add(huXingPicLst.get(i * 2  + 1));
+        //        // 家居用品
+        ViewPager vpJiaJuYongPin = (ViewPager) findViewById(R.id.vp_jiajuyongpin);
+        ArrayList<fragmentFangYuanZhaoPianGroup.PicList> jiaJuYongPinPicLst = new ArrayList<> (
+                Arrays.asList(
+                        new fragmentFangYuanZhaoPianGroup.PicList("家居用品图一", R.drawable.huxingtu1, false),
+                        new fragmentFangYuanZhaoPianGroup.PicList("家居用品图二", R.drawable.huxingtu2, false)
+                )
+        );
+        fillPicGroupInfo(vpJiaJuYongPin, jiaJuYongPinPicLst);
 
-            fragment.setPicList(list);
-            fragLstHuXing.add(fragment);
-        }
-        mVPHuXing.setAdapter(new PicFragStatePageAdapter(getSupportFragmentManager(), fragLstHuXing));
-        mVPHuXing.setCurrentItem(1);
+        //        // 电器
+        ViewPager vpDianQi = (ViewPager) findViewById(R.id.vp_dianqi);
+        ArrayList<fragmentFangYuanZhaoPianGroup.PicList> dianQiPicLst = new ArrayList<> (
+                Arrays.asList(
+                        new fragmentFangYuanZhaoPianGroup.PicList("电器图一", R.drawable.huxingtu1, false),
+                        new fragmentFangYuanZhaoPianGroup.PicList("电器图二", R.drawable.huxingtu2, false)
+                )
+        );
+        fillPicGroupInfo(vpDianQi, dianQiPicLst);
 
-
-//        // 房间结构
-//        ArrayList<fragmentFangYuanZhaoPianGroup.PicList> fangJianJieGouLst = new ArrayList<fragmentFangYuanZhaoPianGroup.PicList> (
-//                Arrays.asList(
-//                        new fragmentFangYuanZhaoPianGroup.PicList("房间结构图一", R.drawable.huxingtu1),
-//                        new fragmentFangYuanZhaoPianGroup.PicList("房间结构图二", R.drawable.huxingtu2)
-//                )
-//        );
-//        fragmentFangYuanZhaoPianGroup.Group fangJianJieGouGroup = new fragmentFangYuanZhaoPianGroup.Group("房间结构", fangJianJieGouLst);
-//
-//        fragmentFangYuanZhaoPianGroup fraFangJianJieGou = new fragmentFangYuanZhaoPianGroup();
-//        fraFangJianJieGou.setGroup(fangJianJieGouGroup);
-//        fragTransaction.replace(R.id.fl_fangjianjiegou, fraFangJianJieGou);
-//
-//        // 家居用品
-//        ArrayList<fragmentFangYuanZhaoPianGroup.PicList> jiaJuYongPinLst = new ArrayList<fragmentFangYuanZhaoPianGroup.PicList> (
-//                Arrays.asList(
-//                        new fragmentFangYuanZhaoPianGroup.PicList("家居用品图一", R.drawable.huxingtu1),
-//                        new fragmentFangYuanZhaoPianGroup.PicList("家居用品图二", R.drawable.huxingtu2)
-//                )
-//        );
-//        fragmentFangYuanZhaoPianGroup.Group jiaJuYongPinGroup = new fragmentFangYuanZhaoPianGroup.Group("家居用品", jiaJuYongPinLst);
-//
-//        fragmentFangYuanZhaoPianGroup fraJiaJuYongPin = new fragmentFangYuanZhaoPianGroup();
-//        fraJiaJuYongPin.setGroup(jiaJuYongPinGroup);
-//        fragTransaction.replace(R.id.fl_jiajuyongpin, fraJiaJuYongPin);
-//
-//        // 电器
-//        ArrayList<fragmentFangYuanZhaoPianGroup.PicList> dianQiLst = new ArrayList<fragmentFangYuanZhaoPianGroup.PicList> (
-//                Arrays.asList(
-//                        new fragmentFangYuanZhaoPianGroup.PicList("电器图一", R.drawable.huxingtu1),
-//                        new fragmentFangYuanZhaoPianGroup.PicList("电器图二", R.drawable.huxingtu2)
-//                )
-//        );
-//        fragmentFangYuanZhaoPianGroup.Group dianQiGroup = new fragmentFangYuanZhaoPianGroup.Group("电器", dianQiLst);
-//
-//        fragmentFangYuanZhaoPianGroup fraDianQi = new fragmentFangYuanZhaoPianGroup();
-//        fraDianQi.setGroup(dianQiGroup);
-//        fragTransaction.replace(R.id.fl_dianqi, fraDianQi);
-//        //
-
-        fragTransaction.commit();
     }
 
     public void onViewClick(View v) {
@@ -131,5 +94,21 @@ public class Activity_fangyuan_zhaopian extends AppCompatActivity {
         public int getCount() {
             return mLst.size();
         }
+    }
+
+    private void fillPicGroupInfo(ViewPager viewPager, ArrayList<fragmentFangYuanZhaoPianGroup.PicList> picLst) {
+        List<Fragment> fragLst = new ArrayList<Fragment>();
+        int lstCnt = picLst.size() % 2 == 0 ? picLst.size() / 2 : picLst.size() / 2 + 1;
+        for (int i = 0; i <lstCnt; i++) {
+            fragmentFangYuanZhaoPianGroup fragment = new fragmentFangYuanZhaoPianGroup();
+            ArrayList<fragmentFangYuanZhaoPianGroup.PicList> list = new ArrayList();
+            list.add(picLst.get(i * 2));
+            list.add(picLst.get(i * 2  + 1));
+
+            fragment.setPicList(list);
+            fragLst.add(fragment);
+        }
+        viewPager.setAdapter(new PicFragStatePageAdapter(getSupportFragmentManager(), fragLst));
+        viewPager.setCurrentItem(0);
     }
 }
--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/fragmentFangYuanZhaoPianGroup.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/fragmentFangYuanZhaoPianGroup.java
@@ -18,14 +18,18 @@ import java.util.ArrayList;
 public class fragmentFangYuanZhaoPianGroup extends Fragment {
 //    private Group mGroup;
     private  ArrayList<PicList> mPicList;
+    ImageView mIvPicCheckFlag;
+    ImageView mIvPic1CheckFlag;
 
     static class PicList {
         String mDesc;
         int    mDrawable;
+        boolean mIsChecked;
 
-        public PicList(String desc, int drawable) {
+        public PicList(String desc, int drawable, boolean isChecked) {
             mDesc = desc;
             mDrawable = drawable;
+            mIsChecked = isChecked;
         }
     }
 
@@ -52,19 +56,65 @@ public class fragmentFangYuanZhaoPianGroup extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fangyuanzhaopian_group, container, false);
 
-//        TextView tvTitle = (TextView) view.findViewById(R.id.tv_title);
-//        tvTitle.setText(mGroup.mTitle);
-
         ImageView ivPic = (ImageView) view.findViewById(R.id.iv_pic);
         TextView tvPicDes = (TextView) view.findViewById(R.id.tv_pic_desc);
         ivPic.setImageResource(mPicList.get(0).mDrawable);
         tvPicDes.setText("照片说明：" + mPicList.get(0).mDesc);
+        ivPic.setOnClickListener(mPicClicked);
+
+        int visibility = View.INVISIBLE;
+        if (mPicList.get(0).mIsChecked)
+            visibility = View.VISIBLE;
+        mIvPicCheckFlag = (ImageView) view.findViewById(R.id.iv_pic_checkflag);
+        mIvPicCheckFlag.setVisibility(visibility);
+
 
         ImageView ivPic1 = (ImageView) view.findViewById(R.id.iv_pic1);
         TextView tvPicDes1 = (TextView) view.findViewById(R.id.tv_pic_desc1);
         ivPic1.setImageResource(mPicList.get(1).mDrawable);
         tvPicDes1.setText("照片说明：" + mPicList.get(1).mDesc);
+        ivPic1.setOnClickListener(mPicClicked);
+
+        visibility = View.INVISIBLE;
+        if (mPicList.get(1).mIsChecked)
+            visibility = View.VISIBLE;
+        mIvPic1CheckFlag = (ImageView) view.findViewById(R.id.iv_pic1_checkflag);
+        mIvPic1CheckFlag.setVisibility(visibility);
 
         return view;
     }
+
+    View.OnClickListener mPicClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            switch (view.getId()) {
+                case R.id.iv_pic:
+                {
+                    int visibility = View.VISIBLE;
+                    if (mIvPicCheckFlag.getVisibility() == View.VISIBLE)
+                        visibility = View.INVISIBLE;
+
+                    mIvPicCheckFlag.setVisibility(visibility);
+                    boolean bChecked = (visibility == View.VISIBLE);
+                    mPicList.get(0).mIsChecked = bChecked;
+
+                    break;
+                }
+
+                case R.id.iv_pic1:
+                {
+                    int visibility = View.VISIBLE;
+                    if (mIvPic1CheckFlag.getVisibility() == View.VISIBLE)
+                        visibility = View.INVISIBLE;
+
+                    mIvPic1CheckFlag.setVisibility(visibility);
+                    boolean bChecked = (visibility == View.VISIBLE);
+                    mPicList.get(1).mIsChecked = bChecked;
+
+                    break;
+                }
+            }
+        }
+    };
 }
