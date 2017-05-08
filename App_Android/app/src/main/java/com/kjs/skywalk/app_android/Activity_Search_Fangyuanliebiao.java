f8eca3b5ac9fe529c65d1fb1f309da4c68b04f4d
Jackie.Guo <bigbigpigpig@163.com>
2017-05-09 00:21:58 +0800

    <Android APp> 完成 房源列表 页面布局

--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_Search_Fangyuanliebiao.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_Search_Fangyuanliebiao.java
@@ -1,12 +1,17 @@
 package com.kjs.skywalk.app_android;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by admin on 2017/3/22.
@@ -14,6 +19,13 @@ import android.widget.TextView;
 
 public class Activity_Search_Fangyuanliebiao extends Activity {
     private ListView mListView = null;
+
+    private TextView mDisplayType = null;
+    private AdapterFangyuanliebiao mAdapter = null;
+
+    private static final int DISPLAY_GRID = 0;
+    private static final int DISPLAY_LIST= 1;
+    private int mDisplay = DISPLAY_LIST;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +38,36 @@ public class Activity_Search_Fangyuanliebiao extends Activity {
         textView.setGravity(Gravity.BOTTOM);
         searchView.onActionViewCollapsed();
 
-//        mListView = (ListView)findViewById(R.id.listViewHint);
-//        AdapterHouseSearchResult adapter = new AdapterHouseSearchResult(this);
-//        mListView.setAdapter(adapter);
+        mListView = (ListView)findViewById(R.id.listViewSearchResult);
+        mAdapter = new AdapterFangyuanliebiao(this);
+        mAdapter.setDisplayType(mDisplay);
+        mListView.setAdapter(mAdapter);
+
+        mDisplayType = (TextView)findViewById(R.id.textViewDisplayMode);
+        mDisplayType.setOnClickListener(mClickListenerDisplayType);
     }
+
+    private View.OnClickListener mClickListenerDisplayType = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if(mDisplay == DISPLAY_LIST) {
+                mDisplay = DISPLAY_GRID;
+                Drawable drawable = ContextCompat.getDrawable(getApplicationContext(), R.drawable.show_list);
+                drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());
+                mDisplayType.setCompoundDrawables(drawable, null, null, null);
+                mDisplayType.setText("小图模式");
+            } else if(mDisplay == DISPLAY_GRID) {
+                mDisplay = DISPLAY_LIST;
+                Drawable drawable = ContextCompat.getDrawable(getApplicationContext(), R.drawable.show_grid);
+                drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());
+                mDisplayType.setCompoundDrawables(drawable, null, null, null);
+                mDisplayType.setText("大图模式");
+            }
+            mAdapter.setDisplayType(mDisplay);
+            mListView.setAdapter(mAdapter);
+            mAdapter.notifyDataSetChanged();
+        }
+    };
 
     public void onViewClick(View v) {
         switch (v.getId()) {
