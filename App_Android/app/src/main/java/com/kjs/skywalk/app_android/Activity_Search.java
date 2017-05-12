1f9651e92311064ceffd8566e6bd3f63de646c6c
Jackie.Guo <bigbigpigpig@163.com>
2017-05-12 17:59:53 +0800

    <Android App> 搜索小区页面, 没有输入时显示最近搜索过的小区名称, 当有字符输入时,显示匹配的小区名称

--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_Search.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_Search.java
@@ -1,12 +1,16 @@
 package com.kjs.skywalk.app_android;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by admin on 2017/3/22.
@@ -14,21 +18,73 @@ import android.widget.TextView;
 
 public class Activity_Search extends Activity {
     private ListView mListView = null;
+    private LinearLayout mContainer = null;
+    private LinearLayout mContainerEmpty = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
         SearchView searchView = (SearchView)findViewById(R.id.search_view);
+
         int id = searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
         TextView textView = (TextView) searchView.findViewById(id);
         textView.setTextSize(14);
         textView.setGravity(Gravity.BOTTOM);
-        searchView.onActionViewCollapsed();
 
-        mListView = (ListView)findViewById(R.id.listViewHint);
+        if (searchView != null) {
+            try {
+                Class<?> argClass = searchView.getClass();
+                Field ownField = argClass.getDeclaredField("mSearchPlate");
+                ownField.setAccessible(true);
+                View mView = (View) ownField.get(searchView);
+                mView.setBackgroundColor(Color.TRANSPARENT);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        mListView = (ListView)findViewById(R.id.listViewResult);
         AdapterHouseSearchResult adapter = new AdapterHouseSearchResult(this);
         mListView.setAdapter(adapter);
+
+        mContainer = (LinearLayout)findViewById(R.id.searchResultContainer);
+        mContainerEmpty = (LinearLayout)findViewById(R.id.searchResultEmptyContainer);
+
+        mContainer.setVisibility(View.GONE);
+        mContainerEmpty.setVisibility(View.VISIBLE);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if(newText.equals("")) {
+                    mContainer.setVisibility(View.GONE);
+                    mContainerEmpty.setVisibility(View.VISIBLE);
+                } else {
+                    if(!mContainer.isShown()) {
+                        mContainer.setVisibility(View.VISIBLE);
+                        mContainerEmpty.setVisibility(View.GONE);
+                    }
+                }
+
+                return true;
+            }
+        });
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        SearchView searchView = (SearchView)findViewById(R.id.search_view);
+        int id = searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+        searchView.onActionViewExpanded();
+        searchView.setIconifiedByDefault(false);
     }
 
     public void onViewClick(View v) {
