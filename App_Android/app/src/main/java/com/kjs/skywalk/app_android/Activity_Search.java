de66a621d2dc5bddc0cf7e1f89da38fdda9faa31
Jackie.Guo <bigbigpigpig@163.com>
2017-05-16 23:44:08 +0800

    <Android App> 完成搜索页面的最近搜索和猜你喜欢 的布局

--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_Search.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_Search.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -15,6 +16,7 @@ import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 
@@ -29,12 +31,16 @@ public class Activity_Search extends Activity {
     private LinearLayout mContainerEmpty = null;
     private ArrayList<String> mHistory = new ArrayList<String>();
     private ArrayList<String> mGuess = new ArrayList<String>();
+    private int mActScreenWidth = 1080;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
+        DisplayMetrics metric = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metric);
+        mActScreenWidth = metric.widthPixels;
+        mActScreenWidth -= 100;
         SearchView searchView = (SearchView)findViewById(R.id.search_view);
 
         int id = searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
@@ -65,6 +71,7 @@ public class Activity_Search extends Activity {
         mContainerEmpty.setVisibility(View.VISIBLE);
 
         initHistory();
+        initGuess();
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -108,44 +115,83 @@ public class Activity_Search extends Activity {
         }
     }
 
-    private TextView createTextItem(String text) {
-        int paddingLeft = 10;
-        int paddingRight = 10;
-        int paddingTop = 10;
-        int paddingBottom = 10;
-        int marginLeft = 15;
-        int marginRight = 15;
-        int marginTop = 15;
-        int marginBottom = 15;
+    class TextItem {
+        public int mTextViewWidth;
+        public TextView mView;
+    }
 
+    private TextItem createTextItem(String text) {
+        int paddingLeft, paddingRight, paddingTop, paddingBottom;
+        int marginLeft, marginRight, marginTop, marginBottom;
+        paddingLeft = paddingRight = 25;
+        paddingTop = paddingBottom = 18;
+        marginLeft = marginRight = marginTop = marginBottom = 11;
+
+        TextItem item = new TextItem();
         TextView textView = new TextView(this);
         textView.setText(text);
         int color = ContextCompat.getColor(this, R.color.colorTextNormal);
         textView.setTextColor(color);
-        Drawable drawable = ContextCompat.getDrawable(this, R.drawable.rounded_rect_background_all_gray);
+        Drawable drawable = ContextCompat.getDrawable(this, R.drawable.rounded_rect_background_f4f4f4);
         textView.setBackground(drawable);
         textView.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        layoutParams.setMargins(15, 15, 15, 15);
+        layoutParams.setMargins(marginLeft, marginTop, marginRight, marginBottom);
         textView.setLayoutParams(layoutParams);
         float size = textView.getPaint().measureText(text);
-        Log.i(TAG, "with = " + size);
-        return textView;
+
+        item.mView = textView;
+        item.mTextViewWidth = (int)size + paddingLeft + paddingRight + marginLeft + marginRight;
+        return item;
     }
 
     private void initGuess() {
-        mHistory.add("中环时代");
-        mHistory.add("新时代");
-        mHistory.add("世茂蝶湖湾");
-        mHistory.add("绿地21新城");
-        mHistory.add("万科魅力花园MIXTOWN");
-        mHistory.add("印象欧洲");
-        mHistory.add("中冶昆廷");
-        mHistory.add("香溢紫君");
-        mHistory.add("两岸新天地");
-        mHistory.add("中汇爱丁堡");
+        mGuess.add("中环时代");
+        mGuess.add("中冶昆廷");
+        mGuess.add("香溢紫君");
+        mGuess.add("印象欧洲");
+        mGuess.add("新时代");
+        mGuess.add("世茂蝶湖湾");
+        mGuess.add("绿地21新城");
+        mGuess.add("万科魅力花园MIXTOWN");
+        mGuess.add("两岸新天地");
+        mGuess.add("中汇爱丁堡");
 
+        ArrayList<String> list = mGuess;
+        LinearLayout container = (LinearLayout)findViewById(R.id.layoutGuess);
+        while(list.size() != 0) {
+            int count = addToContainer(container, list);
+            for(int i = 0; i < count; i ++) {
+                list.remove(0);
+            }
+        }
+    }
 
+    private int addToContainer(LinearLayout container, ArrayList<String> list) {
+        if(list.size() == 0) {
+            return 0;
+        }
+
+        LinearLayout layout = new LinearLayout(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        layout.setLayoutParams(layoutParams);
+        layout.setOrientation(LinearLayout.HORIZONTAL);
+
+        int totalWidth = 0;
+        int count = 0;
+        for(int i = 0; i < list.size(); i ++) {
+            TextItem item = createTextItem(list.get(i));
+            if(totalWidth + item.mTextViewWidth <= mActScreenWidth) {
+                layout.addView(item.mView);
+                totalWidth += item.mTextViewWidth;
+                count += 1;
+            } else {
+                break;
+            }
+        }
+
+        container.addView(layout);
+        return count;
     }
 
     private void initHistory() {
@@ -160,19 +206,13 @@ public class Activity_Search extends Activity {
         mHistory.add("中环时代");
         mHistory.add("新时代");
 
-        LinearLayout layout = new LinearLayout(this);
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        layout.setLayoutParams(layoutParams);
-        layout.setOrientation(LinearLayout.HORIZONTAL);
+        ArrayList<String> list = mHistory;
         LinearLayout container = (LinearLayout)findViewById(R.id.layoutRecent);
-        container.addView(layout);
-//        for(int i = 0; i < mHistory.size(); i ++) {
-        for(int i = 0; i < 3; i ++) {
-            TextView view = createTextItem(mHistory.get(i));
-            layout.addView(view);
-
-            int width = view.getWidth();
-            Log.i(TAG, "with = " + width);
+        while(list.size() != 0) {
+            int count = addToContainer(container, list);
+            for(int i = 0; i < count; i ++) {
+                list.remove(0);
+            }
         }
     }
 }
