7e0fee31c6e68f46acd52a1639f11c49b88b4e49
Jackie.Guo <bigbigpigpig@163.com>
2017-05-16 11:55:17 +0800

    <Android App>委托确认 页面加载失败时, 显示重新加载按钮, 点击可以重新加载

--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_Weituoqueren.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_Weituoqueren.java
@@ -1,29 +1,39 @@
 package com.kjs.skywalk.app_android;
 
 import android.app.Activity;
-import android.app.Dialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
-import android.widget.Toast;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * Created by admin on 2017/3/16.
  */
 
 public class Activity_Weituoqueren extends Activity {
+    private final String TAG = "Weiguoqueren";
     private WebView mWebView = null;
     private ProgressBar mProgress = null;
+    private RelativeLayout mProgressContainer = null;
+    //private String mURL = "http://www.baidu.com/";
+    private String mURL = "file:////sdcard/test.html";
+    private String mErrorPage = "file:////sdcard/error.html";
+    private final int MSG_HIDE_PROGRESS_BAR = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tijiaoweituo);
 
         mProgress = (ProgressBar)findViewById(R.id.progressBar);
+        mProgressContainer = (RelativeLayout)findViewById(R.id.progressContainer);
         mWebView = (WebView)findViewById(R.id.webView);
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new WebViewClient() {
@@ -38,17 +48,27 @@ public class Activity_Weituoqueren extends Activity {
             public void onProgressChanged(WebView view, int progress) {
                 mProgress.setProgress(progress);
                 if(progress == 100) {
-                    mProgress.setVisibility(View.GONE);
+                    myHandler.sendEmptyMessageDelayed(MSG_HIDE_PROGRESS_BAR, 500);
                 }
                 else
                 {
-                    mProgress.setVisibility(View.VISIBLE);
+                    mProgressContainer.setVisibility(View.VISIBLE);
                 }
             }
         });
 
-        //mWebView.loadUrl("http://www.baidu.com/");
-        mWebView.loadUrl("file:////sdcard/test.html");
+        mWebView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Log.e(TAG, description);
+                mWebView.removeAllViews();
+                //mWebView.loadUrl(mErrorPage);
+                TextView reload = (TextView)findViewById(R.id.clickReloadView);
+                reload.setVisibility(View.VISIBLE);
+            }
+
+//            public void onReceivedError(WebView viewWeb, WebResourceRequest request, WebResourceError error) {
+//            }
+        });
     }
 
     public void onViewClick(View v) {
@@ -58,6 +78,29 @@ public class Activity_Weituoqueren extends Activity {
                 finish();
             }
             break;
+            case R.id.clickReloadView:
+            {
+                TextView reload = (TextView)findViewById(R.id.clickReloadView);
+                reload.setVisibility(View.INVISIBLE);
+                mWebView.loadUrl(mURL);
+            }
         }
     }
+
+    protected void onResume() {
+        super.onResume();
+        mWebView.removeAllViews();
+        mWebView.loadUrl(mURL);
+    }
+
+    Handler myHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_HIDE_PROGRESS_BAR:
+                    mProgressContainer.setVisibility(View.INVISIBLE);
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
 }
