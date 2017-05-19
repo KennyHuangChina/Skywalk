4d51accef7898da9e9b110fa2cd1155c52a6c83f
Jackie.Guo <bigbigpigpig@163.com>
2017-05-19 16:05:08 +0800

    <Android App>租售委托完成页面, 点击 上传"上传房源图片"或"完善房源信息"跳转至响应页面, 完成后返回MainActivity

--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_Zushouweituo_Finish.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_Zushouweituo_Finish.java
@@ -1,6 +1,7 @@
 package com.kjs.skywalk.app_android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -16,13 +17,36 @@ public class Activity_Zushouweituo_Finish extends Activity {
         setContentView(R.layout.activity__zushouweituo__finish);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        doFinish();
+    }
+
+    private void  doFinish() {
+        Intent intent =new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+
+        finish();
+    }
+
     public void onViewClick(View v) {
         switch (v.getId()) {
             case R.id.tv_close:
             {
-                finish();
+                doFinish();
+                break;
             }
-            break;
+            case R.id.improveContainer: {
+                startActivityForResult(new Intent(Activity_Zushouweituo_Finish.this, Activity_fangyuan_guanli.class), 0);
+                break;
+            }
+            case R.id.uploadContainer: {
+                startActivityForResult(new Intent(Activity_Zushouweituo_Finish.this, Activity_fangyuan_zhaopian.class), 1);
+                break;
+            }
         }
     }
 }
