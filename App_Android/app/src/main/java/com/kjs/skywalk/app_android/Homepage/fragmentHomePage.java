5900e357148d075b15623e526063b2ac40cfd49d
sailorzhou <shuguang168@sina.com>
2017-05-28 17:01:42 +0800

    [Android App] 获取推荐房源信息

--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/Homepage/fragmentHomePage.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Homepage/fragmentHomePage.java
@@ -14,13 +14,17 @@ import android.widget.ListView;
 import com.kjs.skywalk.app_android.Apartment.Activity_ApartmentDetail;
 import com.kjs.skywalk.app_android.R;
 import com.kjs.skywalk.app_android.commonFun;
+import com.kjs.skywalk.app_android.kjsLogUtil;
 import com.kjs.skywalk.communicationlibrary.CommandManager;
 import com.kjs.skywalk.communicationlibrary.CommunicationError;
 import com.kjs.skywalk.communicationlibrary.CommunicationInterface;
 import com.kjs.skywalk.communicationlibrary.IApiResults;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
+import static android.content.ContentValues.TAG;
+import static com.kjs.skywalk.communicationlibrary.CommunicationInterface.CmdID.CMD_GET_BRIEF_PUBLIC_HOUSE_INFO;
 import static com.kjs.skywalk.communicationlibrary.CommunicationInterface.CmdID.CMD_GET_HOUSE_INFO;
 import static com.kjs.skywalk.communicationlibrary.CommunicationInterface.CmdID.CMD_GET_HOUSE_LIST;
 
@@ -55,10 +59,14 @@ public class fragmentHomePage extends Fragment {
         // get recommend house info
 
         CommandManager CmdMgr = new CommandManager(getActivity(), mCmdListener, mProgreessListener);
-        int result = CmdMgr.GetHouseList(0, 0, 0);
+        int result = CmdMgr.GetHouseList(1, 0, 4);
         if(result != CommunicationError.CE_ERROR_NO_ERROR) {
 
         }
+
+        CmdMgr.GetHouseInfo(2, false);
+
+        CmdMgr.GetBriefPublicHouseInfo(2);
 
         return view;
     }
@@ -67,18 +75,63 @@ public class fragmentHomePage extends Fragment {
         @Override
         public void onCommandFinished(int command, IApiResults.ICommon result) {
             if (null == result) {
-                Log.w("ServerAPI", "result is null");
+                kjsLogUtil.w("result is null");
                 return;
             }
 //            mResultString = "Request: " + command + "\n" + result.DebugString();
 
             if (CommunicationError.CE_ERROR_NO_ERROR != result.GetErrCode()) {
-                Log.e("ServerAPI", "Command:"+ command + " finished with error: " + result.GetErrDesc());
+                kjsLogUtil.e("Command:" + command + " finished with error: " + result.GetErrDesc());
                 return;
             }
 
+            kjsLogUtil.i("=============================> Request: " + command + "\n" + result.DebugString());
+
+
             if (command == CMD_GET_HOUSE_LIST) {
                 IApiResults.IResultList resultList = (IApiResults.IResultList) result;
+                int nTotal = resultList.GetTotalNumber();
+                int nFetch = resultList.GetFetchedNumber();
+                if (nFetch > 0) {
+                    ArrayList<Object> houseIDs = resultList.GetList();
+                    int size = houseIDs.size();
+                    for (Object houseID : houseIDs) {
+                        kjsLogUtil.i("house_id: " + houseID);
+                    }
+                    int a = 0;
+                }
+            }
+
+            if (command == CMD_GET_HOUSE_INFO) {
+                IApiResults.IGetHouseInfo houseInfoRes = (IApiResults.IGetHouseInfo) result;
+
+                kjsLogUtil.i("Floorthis: " + houseInfoRes.Floorthis());
+                kjsLogUtil.i("FloorTotal: " + houseInfoRes.FloorTotal());
+                kjsLogUtil.i("FloorDesc: " + houseInfoRes.FloorDesc());
+                kjsLogUtil.i("BuildingNo: " + houseInfoRes.BuildingNo());
+                kjsLogUtil.i("toString: " + houseInfoRes.toString());
+//                kjsLogUtil.i("FloorTotal: " + houseInfoRes.FloorTotal());
+//                kjsLogUtil.i("FloorTotal: " + houseInfoRes.FloorTotal());
+//                kjsLogUtil.i("FloorTotal: " + houseInfoRes.FloorTotal());
+//                kjsLogUtil.i("FloorTotal: " + houseInfoRes.FloorTotal());
+//                kjsLogUtil.i("FloorTotal: " + houseInfoRes.FloorTotal());
+
+            }
+
+            if (command == CMD_GET_BRIEF_PUBLIC_HOUSE_INFO) {
+                IApiResults.IHouseDigest houseDigestRes = (IApiResults.IHouseDigest) result;
+                kjsLogUtil.i("GetHouseId: " + houseDigestRes.GetHouseId());
+                kjsLogUtil.i("GetProperty: " + houseDigestRes.GetProperty());
+                kjsLogUtil.i("GetPropertyAddr: " + houseDigestRes.GetPropertyAddr());
+                kjsLogUtil.i("GetBedrooms: " + houseDigestRes.GetBedrooms());
+                kjsLogUtil.i("GetLivingrooms: " + houseDigestRes.GetLivingrooms());
+                kjsLogUtil.i("GetBathrooms: " + houseDigestRes.GetBathrooms());
+                kjsLogUtil.i("GetAcreage: " + houseDigestRes.GetAcreage());
+                kjsLogUtil.i("GetRental: " + houseDigestRes.GetRental());
+                kjsLogUtil.i("GetPricing: " + houseDigestRes.GetPricing());
+                kjsLogUtil.i("GetCoverImage: " + houseDigestRes.GetCoverImage());
+
+
             }
         }
     };
