5514a208987625957d8a7b84808462d7a136fef1
sailorzhou <shuguang168@sina.com>
2017-06-05 23:12:05 +0800

    [Android App] 改变homepage中房源group的布局和定义。

--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/Homepage/homepage_apartment_listitem_adapter.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Homepage/homepage_apartment_listitem_adapter.java
@@ -5,6 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.kjs.skywalk.app_android.ClassDefine;
@@ -46,7 +49,20 @@ public class homepage_apartment_listitem_adapter extends BaseAdapter {
     }
 
     class ViewHolder {
-        TextView tvContentName;
+        TextView tvGroupTitle;
+        TextView tvGroupCount;
+        RelativeLayout rlApartment1;
+        ImageView ivApartment1_thumb;
+        ImageView ivApartment1_flag;
+        TextView tvApartment1_name;
+        TextView tvApartment1_desc;
+        LinearLayout llApartment1_tag;
+        RelativeLayout rlApartment2;
+        ImageView ivApartment2_thumb;
+        ImageView ivApartment2_flag;
+        TextView tvApartment2_name;
+        TextView tvApartment2_desc;
+        LinearLayout llApartment2_tag;
     }
 
     @Override
@@ -57,7 +73,23 @@ public class homepage_apartment_listitem_adapter extends BaseAdapter {
             view = LayoutInflater.from(mContext).inflate(R.layout.homepage_listitem, null);
 
             holder = new ViewHolder();
-            holder.tvContentName = (TextView) view.findViewById(R.id.tv_info_title);
+            holder.tvGroupTitle = (TextView) view.findViewById(R.id.tv_group_title);
+            holder.tvGroupCount = (TextView) view.findViewById(R.id.tv_group_count);
+
+            holder.rlApartment1 = (RelativeLayout) view.findViewById(R.id.rl_apartment1);
+            holder.ivApartment1_thumb = (ImageView) view.findViewById(R.id.iv_apartment1_thumb);
+            holder.ivApartment1_flag = (ImageView) view.findViewById(R.id.iv_apartment1_flag);
+            holder.tvApartment1_name = (TextView) view.findViewById(R.id.tv_apartment1_name);
+            holder.tvApartment1_desc = (TextView) view.findViewById(R.id.tv_apartment1_desc);
+            holder.llApartment1_tag = (LinearLayout) view.findViewById(R.id.ll_apartment1_tag);
+
+            holder.rlApartment2 = (RelativeLayout) view.findViewById(R.id.rl_apartment2);
+            holder.ivApartment2_thumb = (ImageView) view.findViewById(R.id.iv_apartment2_thumb);
+            holder.ivApartment2_flag = (ImageView) view.findViewById(R.id.iv_apartment2_flag);
+            holder.tvApartment2_name = (TextView) view.findViewById(R.id.tv_apartment2_name);
+            holder.tvApartment2_desc = (TextView) view.findViewById(R.id.tv_apartment2_desc);
+            holder.llApartment2_tag = (LinearLayout) view.findViewById(R.id.ll_apartment2_tag);
+
             view.setTag(holder);
         } else {
             holder = (ViewHolder) view.getTag();
