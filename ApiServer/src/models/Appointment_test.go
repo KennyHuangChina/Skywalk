6a4ba4ff8d6d96c7271e50a97870328f0200aee7
Kenny Huang <2465691298@qq.com>
2017-06-24 22:12:11 +0800

    <API server> MakeAppointment 1. add test code, but not implement yet

---
+++ ApiServer/src/models/Appointment_test.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	// "ApiServer/commdef"
+	// "fmt"
+	// "strconv"
+	// "github.com/astaxie/beego"
+	"testing"
+)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	-- MakeAppointment --
+//
+func Test_MakeAppointment(t *testing.T) {
+	t.Log("Test MakeAppointment, Invalid Arguments")
+	t.Error("NOT Implement")
+}
