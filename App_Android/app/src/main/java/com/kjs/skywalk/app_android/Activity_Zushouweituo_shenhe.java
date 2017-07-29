97a8fb5dfcc6beae64a9b42d0aea3ddc82a8b342
Jackie.Guo <bigbigpigpig@163.com>
2017-07-29 23:35:25 +0800

    <Android App> 房源审核页面 编辑模式 （ongoing）

--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_Zushouweituo_shenhe.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_Zushouweituo_shenhe.java
@@ -1,5 +1,7 @@
 package com.kjs.skywalk.app_android;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -8,6 +10,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class Activity_Zushouweituo_shenhe extends SKBaseActivity {
+
+    private boolean mModifyMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,29 @@ public class Activity_Zushouweituo_shenhe extends SKBaseActivity {
                 finish();
             }
             break;
+            case R.id.textViewModify: {
+                mModifyMode = !mModifyMode;
+                TextView button = (TextView)v;
+                TextView floor = (TextView)findViewById(R.id.textViewFloor);
+                if(mModifyMode) {
+                    Drawable drawable = ContextCompat.getDrawable(this, R.drawable.right_n);
+                    drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());
+                    floor.setCompoundDrawablePadding(15);
+                    floor.setCompoundDrawables(null, null, drawable, null);
+
+                    int color = ContextCompat.getColor(this, R.color.colorText);
+                    floor.setTextColor(color);
+
+                    button.setText("完成");
+                } else {
+                    floor.setCompoundDrawables(null, null, null, null);
+                    int color = ContextCompat.getColor(this, R.color.colorTextNormal);
+                    floor.setTextColor(color);
+                    button.setText("修改");
+                }
+
+                break;
+            }
         }
     }
 }
