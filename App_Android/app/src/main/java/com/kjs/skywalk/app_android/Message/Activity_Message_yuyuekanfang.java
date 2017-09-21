0c1ebd3d1dce7b02537b1afcabcdfe39525db789
sailorzhou <shuguang168@sina.com>
2017-09-21 23:27:56 +0800

    [Android App] 消息详情 --- 预约看房

---
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Message/Activity_Message_yuyuekanfang.java
@@ -0,0 +1,15 @@
+package com.kjs.skywalk.app_android.Message;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kjs.skywalk.app_android.R;
+
+public class Activity_Message_yuyuekanfang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__message_yuyuekanfang);
+    }
+}
