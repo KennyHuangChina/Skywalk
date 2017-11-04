58e6448efc926f539039c3154fec2e9555dea76c
Jackie.Guo <bigbigpigpig@163.com>
2017-11-04 12:15:31 +0800

    <Android App> 房源管理页面，显示审核状态

--- App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_fangyuan_guanli.java
+++ App_Android/app/src/main/java/com/kjs/skywalk/app_android/Activity_fangyuan_guanli.java
@@ -3,18 +3,22 @@ package com.kjs.skywalk.app_android;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.graphics.Color;
+import android.sax.RootElement;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.kjs.skywalk.app_android.Apartment.Activity_ApartmentDetail;
 import com.kjs.skywalk.app_android.Homepage.fragmentHomePage;
 import com.kjs.skywalk.app_android.ClassDefine.IntentExtraKeyValue;
 import com.kjs.skywalk.communicationlibrary.CommandManager;
 import com.kjs.skywalk.communicationlibrary.CommunicationError;
+import com.kjs.skywalk.communicationlibrary.CommunicationInterface;
 import com.kjs.skywalk.communicationlibrary.IApiResults;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -28,6 +32,7 @@ import static com.kjs.skywalk.communicationlibrary.IApiResults.HOUSE_CERT_STAT_W
 
 public class Activity_fangyuan_guanli extends SKBaseActivity {
 
+    private RelativeLayout mContainer = null;
     private fragmentFangYuanGuanLiInfo1 mFragInfo1 = null;
     private fragmentFangYuanGuanLiInfo2 mFragInfo2 = null;
     private fragmentFangYuanGuanLiInfo3 mFragInfo3 = null;
@@ -37,19 +42,47 @@ public class Activity_fangyuan_guanli extends SKBaseActivity {
     private String mInfo2 = "";
     IApiResults.IHouseCertDigestInfo mCertDigestInfo = null;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fangyuan_guanli);
+
+        mContainer = (RelativeLayout)findViewById(R.id.container);
 
         mFragInfo1 = new fragmentFangYuanGuanLiInfo1();
         mFragInfo2 = new fragmentFangYuanGuanLiInfo2();
         mFragInfo3 = new fragmentFangYuanGuanLiInfo3();
+    }
 
-        mCmdMgr = CommandManager.getCmdMgrInstance(this, this, this);
-        mCmdMgr.GetHouseInfo(mHouseId, false);
-        kjsLogUtil.i("GetHouseInfo: " + mHouseId);
-        mCmdMgr.GetBriefPublicHouseInfo(mHouseId);
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        getHouseInfo();
+    }
+
+    private void getHouseInfo() {
+        CommunicationInterface.CICommandListener listener = new CommunicationInterface.CICommandListener() {
+            @Override
+            public void onCommandFinished(int command, IApiResults.ICommon iResult) {
+                if (null == iResult) {
+                    kjsLogUtil.w("result is null");
+                    return;
+                }
+
+                if (CommunicationError.CE_ERROR_NO_ERROR != iResult.GetErrCode()) {
+                    commonFun.showToast_info(getApplicationContext(), mContainer, "获取房屋信息失败:" + iResult.GetErrDesc());
+                    return;
+                }
+
+                if (command == CMD_GET_HOUSE_INFO) {
+                    updateHouseInfo((IApiResults.IGetHouseInfo) iResult);
+                }
+            }
+        };
+
+        CommandManager manager = CommandManager.getCmdMgrInstance(this, listener, this);
+        manager.GetHouseInfo(mHouseId, true);
     }
 
     public void onViewClick(View v) {
@@ -118,63 +151,52 @@ public class Activity_fangyuan_guanli extends SKBaseActivity {
     @Override
     public void onCommandFinished(int command, IApiResults.ICommon iResult) {
         kjsLogUtil.i("Activity_ApartmentDetail::onCommandFinished");
-        if (null == iResult) {
-            kjsLogUtil.w("result is null");
-            return;
-        }
-        kjsLogUtil.i(String.format("[command: %d] --- %s", command, iResult.DebugString()));
-        if (CommunicationError.CE_ERROR_NO_ERROR != iResult.GetErrCode()) {
-            kjsLogUtil.e("Command:" + command + " finished with error: " + iResult.GetErrDesc());
-            return;
-        }
-
-        if (command == CMD_GET_HOUSE_INFO) {
-            updateHouseInfo((IApiResults.IGetHouseInfo) iResult);
-        }
-        if(command == CMD_GET_BRIEF_PUBLIC_HOUSE_INFO) {
-            mCertDigestInfo = (IApiResults.IHouseCertDigestInfo)iResult;
-            updateFragmentInfo(mCertDigestInfo);
-        }
 
     }
 
-    private void updateHouseInfo(final IApiResults.IGetHouseInfo IHouseInfo) {
+    private void updateHouseInfo(final IApiResults.IGetHouseInfo houseInfo) {
         this.runOnUiThread(new Runnable() {
             @Override
             public void run() {
 
-                mInfo2 = commonFun.getHouseTypeString(IHouseInfo.Bedrooms(), IHouseInfo.Livingrooms(), IHouseInfo.Bathrooms());
+                mInfo2 = commonFun.getHouseTypeString(houseInfo.Bedrooms(), houseInfo.Livingrooms(), houseInfo.Bathrooms());
                 mInfo2 += " | ";
-                mInfo2 += String.format("%d m²", IHouseInfo.Acreage() / 100);
+                mInfo2 += String.format("%d m²", houseInfo.Acreage() / 100);
                 mInfo2 += " | ";
-                mInfo2 += String.format("%d/%d F", IHouseInfo.Floorthis(), IHouseInfo.FloorTotal());
+                mInfo2 += String.format("%d/%d F", houseInfo.Floorthis(), houseInfo.FloorTotal());
 
-
-//                // 1: wait for rent, 2: rented, 3: Due, open for ordering
-//                IHouseInfo.RentStat();
-//                public interface IHouseCertInfo {
-//                    int     CertStat();     // certification stat. 1: wait for certification; 2: Certitication passed; 3: Certification failed
-//                    Date CertTime();
-//                    String  CertDesc();
-//                }
-
-
+                IApiResults.IHouseCertDigestInfo certInfo = (IApiResults.IHouseCertDigestInfo)houseInfo;
+                updateFragment(certInfo);
             }
         });
     }
 
-    private void updateFragmentInfo(IApiResults.IHouseCertDigestInfo info) {
-
+    private void updateFragment(IApiResults.IHouseCertDigestInfo info) {
+        FragmentTransaction fragTransaction = getFragmentManager().beginTransaction();
         switch (info.CertStat()) {
             case HOUSE_CERT_STAT_WAIT:
+                fragTransaction.replace(R.id.info_container, mFragInfo1);mFragInfo1.setHouseLocation(mHouseLocation);
+                mFragInfo1.setHouseInfo(mInfo2);
+                mFragInfo1.setHouseLocation(mHouseLocation);
                 break;
             case HOUSE_CERT_STAT_PASSED:
+                mFragInfo3.setHouseLocation(mHouseLocation);
+                mFragInfo3.setHouseInfo(mInfo2);
+                Date date = info.CertTime();
+                //SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd");
+                String t = format.format(date);
+                mFragInfo3.setPassTime(t);
+                fragTransaction.replace(R.id.info_container, mFragInfo3);
                 break;
             case HOUSE_CERT_STAT_FAILED:
+                mFragInfo2.setHouseInfo(mInfo2);
+                mFragInfo2.setHouseLocation(mHouseLocation);
+                fragTransaction.replace(R.id.info_container, mFragInfo2);
                 break;
         }
-        if (mFragInfo1 != null)
-//            mFragInfo1.updateInfo(mHouseLocation, mInfo2);
+
+        fragTransaction.commit();
     }
 
 }
