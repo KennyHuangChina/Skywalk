19e4c33c642c5e7fd0d229e3591e2f9d01f21c6f
Kenny.Huang <2465691298@qq.com>
2017-12-29 10:53:48 +0800

    <Comm Lib> 将 Arg 移到 Command 内作为内部类

--- moduleCommunication/android/Communication/communicationlibrary/src/main/java/com/kjs/skywalk/communicationlibrary/CmdGetBehalfHouses.java
+++ moduleCommunication/android/Communication/communicationlibrary/src/main/java/com/kjs/skywalk/communicationlibrary/CmdGetBehalfHouses.java
@@ -5,7 +5,7 @@ import android.util.Log;
 
 import org.json.JSONObject;
 
-import java.util.HashMap;
+import static com.kjs.skywalk.communicationlibrary.IApiArgs.*;
 
 /**
  * Created by kenny on 2017/3/12.
@@ -38,5 +38,64 @@ class CmdGetBehalfHouses extends CommunicationBase {
     public IApiResults.ICommon doParseResult(int nErrCode, JSONObject jObject) {
         ResGetHouseList result = new ResGetHouseList(nErrCode, jObject);
         return result;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////
+    //
+    //
+    class ApiArgsGetBehalfHouseList extends ApiArgsBase implements IApiArgs.IArgsGetBehalfList {
+        private int mType       = IApiArgs.AGENT_HOUSE_ALL;    // AGENT_HOUSE_xxx
+        private int mBegin      = 0;
+        private int mFetchCnt   = 0;
+
+        public ApiArgsGetBehalfHouseList(int type, int bgn, int cnt) {
+            mType       = type;
+            mBegin      = bgn;
+            mFetchCnt   = cnt;
+        }
+
+        @Override
+        public boolean checkArgs() {
+            if (mType < AGENT_HOUSE_ALL || mType > AGENT_HOUSE_END) {
+                Log.e(TAG, "mType:" + mType);
+                return false;
+            }
+            if (mBegin < 0) {
+                Log.e(TAG, "begin:" + mBegin);
+                return false;
+            }
+            if (mFetchCnt < 0) {
+                Log.e(TAG, "mFetchCnt:" + mFetchCnt);
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public boolean isEqual(IApiArgs.IArgsBase arg2) {
+            if (!super.isEqual(arg2)) {
+                return false;
+            }
+            ApiArgsGetBehalfHouseList arg2chk = (ApiArgsGetBehalfHouseList)arg2;
+            if (mType != arg2chk.mType || mBegin != arg2chk.mBegin || mFetchCnt != mFetchCnt) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public int getType() {
+            return mType;
+        }
+
+        @Override
+        public int getBeginPosi() {
+            return mBegin;
+        }
+
+        @Override
+        public int getFetchCnt() {
+            return mFetchCnt;
+        }
     }
 }
