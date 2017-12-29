fc88c78418af4f5c79840a1fb114a3df17087b83
Kenny.Huang <2465691298@qq.com>
2017-12-29 15:37:57 +0800

    <Commu Lib> 将 IApiArgs.IArgsGetMsgList 移动到 CmdGetSysMsgList, 作为他的内部类

--- moduleCommunication/android/Communication/communicationlibrary/src/main/java/com/kjs/skywalk/communicationlibrary/CmdGetXPicList.java
+++ moduleCommunication/android/Communication/communicationlibrary/src/main/java/com/kjs/skywalk/communicationlibrary/CmdGetXPicList.java
@@ -7,6 +7,11 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 
+import static com.kjs.skywalk.communicationlibrary.IApiArgs.PIC_SIZE_ALL;
+import static com.kjs.skywalk.communicationlibrary.IApiArgs.PIC_SIZE_Large;
+import static com.kjs.skywalk.communicationlibrary.IApiArgs.PIC_TYPE_SUB_HOUSE_BEGIN;
+import static com.kjs.skywalk.communicationlibrary.IApiArgs.PIC_TYPE_SUB_HOUSE_END;
+
 /**
  * Created by kenny on 2017/10/28.
  */
@@ -68,5 +73,71 @@ class CmdGetXPicList extends CommunicationBase {
             return false;
         }
         return true;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //      -- API Arguments --
+    //
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    class ApiArgsGetXPiclst extends ApiArgsBase implements IApiArgs.IArgsGetXPicLst {
+
+        private int mXId  = 0;
+        private int mType = -1;     // ref to PIC_TYPE_SUB_HOUSE_xxx
+        private int mSize = -1;     // ref to
+
+        public ApiArgsGetXPiclst(int xid, int type, int size) {
+            mXId    = xid;
+            mType   = type;
+            mSize   = size;
+        }
+
+        @Override
+        public boolean checkArgs() {
+            String Fn = "[checkArgs] ";
+//        return super.checkArgs();
+            if (mXId <= 0) {
+                Log.e(TAG, "xid:" + mXId);
+                return false;
+            }
+            if (mType < PIC_TYPE_SUB_HOUSE_BEGIN || mType > PIC_TYPE_SUB_HOUSE_END) {
+                Log.e(TAG, "type:" + mType);
+                return false;
+            }
+            if (mSize < PIC_SIZE_ALL || mSize > PIC_SIZE_Large) {
+                Log.e(TAG, "mType:" + mType);
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        public boolean isEqual(IApiArgs.IArgsBase arg2) {
+            if (!super.isEqual(arg2)) {
+                return false;
+            }
+            ApiArgsGetXPiclst arg2_chk = (ApiArgsGetXPiclst)arg2;
+            if (mXId != arg2_chk.mXId || mType != arg2_chk.mType || mSize != arg2_chk.mSize) {
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        public int getXId() {
+            return mXId;
+        }
+
+        @Override
+        public int getSubType() {
+            return mType;
+        }
+
+        @Override
+        public int getSize() {
+            return mSize;
+        }
     }
 }
